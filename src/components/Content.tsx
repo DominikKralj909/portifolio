0c2fc4d45257c7516b58c64594414c8601b21d9a
Dominik Kralj <kraljdominik97@gmail.com>
2025-01-31 19:32:46 +0100

    Added ContentParagraph component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,42 @@
+import ExperienceCard from './ExperienceCard';
+
+const experiences = [
+    {
+        startDate: '01/2025',
+        endDate: 'Present',
+        title: 'Front-End Developer',
+        company: 'Artkod',
+        website: 'https://www.artkod.com/',
+        description: `Working on complex web applications, ensuring high performance, accessibility, and seamless UI/UX.`,
+        technologies: ['React', 'Sass', 'Laravel', 'Typescript', 'CRM']
+    }, {
+        startDate: '01/2021',
+        endDate: '12/2024',
+        title: 'Front-End Engineer',
+        company: 'CloudSense',
+        website: 'https://www.cloudsense.com/',
+        description: `
+			Developed and maintained a robust UI component library tailored for business logic-driven products, 
+			ensuring component consistency across a suite of complex business applications. Worked on mayor business products, learning and integrating new technologies essential to
+			meeting complex product requirements and high reliability standards.
+		`,
+        technologies: ['React', 'Redux', 'Sass', 'Jest/RTL', 'Salesforce', 'Typescript']
+    },  {
+        startDate: '07/2022',
+        endDate: '08/2022',
+        title: 'Front-End Developer Intern',
+        company: 'Perpetuum Mobile',
+        website: 'https://www.perpetuum.hr/en',
+        description: `
+			Designed and implemented a custom website theme using Drupal 9, contributing to client-facing solutions.
+			Actively sought feedback from senior developers, enhancing the theme iteratively and gaining valuable insights into professional web
+			development workflow.
+		`,
+        technologies: ['Drupal', 'HTML5', 'CSS3', 'Javascript']
+    },
+];
+
+
 function Content() {
 	return (
 		<main className="portifolio-content">
@@ -18,6 +57,11 @@ function Content() {
 				<p className="portifolio-content-about-text">
 					Outside of work, you’ll find me hiking through nature trails or spending time with my dog on long walks.
 				</p>
+			</section>
+			<section className="portifolio-content-experience">
+				{experiences.map((exp, index) => (
+					<ExperienceCard key={index} {...exp} />
+				))}
 			</section>
 		</main>
 	);
---
+++ src/components/ContentParagraph.tsx
@@ -0,0 +1,9 @@
+function ContentParagraph ({ text }: { text: string }) {
+	return (
+		<p className="portifolio-content-about-text">
+			{text}
+		</p>
+	);
+}
+
+export default ContentParagraph;
